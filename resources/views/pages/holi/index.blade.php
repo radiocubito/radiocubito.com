25c0d51ad2a3f36683533f3789f8458db071ed3c
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-02-16 14:19:36 -0600

    wip

--- resources/views/pages/holi/index.blade.php
+++ resources/views/pages/holi/index.blade.php
@@ -25,47 +25,66 @@
                             </a>
                         </div>
                     </div>
-                    <div
-                        class="w-[45rem] overflow-hidden rounded-xl border border-zinc-200 bg-slate-50 shadow-xl shadow-zinc-950/20 sm:w-auto lg:w-[67.8125rem]"
-                    >
+                    <div class="-mx-5 max-h-[560px] overflow-hidden border-b px-5">
                         <img src="/images/screenshot-1.png" class="w-full" />
                     </div>
                 </div>
             </div>
         </x-container>
         <x-container>
-            <div class="py-20">
-                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight text-zinc-950">Personaliza tus enlaces cortos</h2>
-                <p class="mt-5 max-w-xl text-lg text-zinc-950/80">
-                    Utiliza tus propios dominios para crear URLs breves y que representan a tu marca o proyecto.
-                </p>
-
-                <div
-                    class="mt-10 w-[45rem] overflow-hidden rounded-xl border border-zinc-200 bg-slate-50 shadow-xl shadow-zinc-950/20 sm:w-auto lg:w-[67.8125rem]"
-                >
-                    <img src="/images/screenshot-2.png" class="w-full" />
+            <div class="py-24">
+                <div class="space-y-12">
+                    <div class="max-w-3xl">
+                        <h2 class="mt-4 text-4xl font-extrabold tracking-tight text-zinc-950">Personaliza tus enlaces cortos</h2>
+                        <p class="mt-5 text-lg text-zinc-950/80">
+                            Utiliza tus propios dominios para crear URLs breves y que representan a tu marca o proyecto.
+                        </p>
+                    </div>
+                    <div class="-mx-5 max-h-[560px] overflow-hidden border-b px-5">
+                        <img src="/images/screenshot-2.png" class="w-full" />
+                    </div>
                 </div>
             </div>
         </x-container>
         <x-container>
-            <div class="py-20">
-                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight text-zinc-950">Previsualización social para tus enlaces</h2>
-                <p class="mt-5 max-w-xl text-lg text-zinc-950/80">
-                    Asegura un impacto visual en tus redes sociales con vistas previas personalizadas para tus URLs acortadas.
-                </p>
-                <div
-                    class="mt-10 w-[45rem] overflow-hidden rounded-xl border border-zinc-200 bg-slate-50 shadow-xl shadow-zinc-950/20 sm:w-auto lg:w-[67.8125rem]"
-                >
-                    <img src="/images/screenshot-3.png" class="w-full" />
+            <div class="py-24">
+                <div class="space-y-12">
+                    <div class="max-w-3xl">
+                        <h2 class="mt-4 text-4xl font-extrabold tracking-tight text-zinc-950">Previsualización social para tus enlaces</h2>
+                        <p class="mt-5 text-lg text-zinc-950/80">
+                            Asegura un impacto visual en tus redes sociales con vistas previas personalizadas para tus URLs acortadas.
+                        </p>
+                    </div>
+                    <div class="-mx-5 max-h-[560px] overflow-hidden border-b px-5">
+                        <img src="/images/screenshot-3.png" class="w-full" />
+                    </div>
                 </div>
             </div>
         </x-container>
         <x-container>
-            <div class="py-20">
-                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight text-zinc-950">Plataforma abierta de enlaces cortos</h2>
-                <p class="mt-5 max-w-xl text-lg text-zinc-950/80">
-                    Software de código abierto que garantiza la libertad de elección y evita dependencias innecesarias.
-                </p>
+            <div class="py-24">
+                <div class="max-w-3xl">
+                    <h2 class="mt-4 text-4xl font-extrabold tracking-tight text-zinc-950">Plataforma abierta de enlaces cortos</h2>
+                    <p class="mt-5 text-lg text-zinc-950/80">
+                        Software de código abierto que garantiza la libertad de elección y evita dependencias innecesarias.
+                    </p>
+                </div>
+            </div>
+        </x-container>
+        <x-container>
+            <div class="pb-24">
+                <div class="rounded-xl bg-zinc-100 p-16">
+                    <div class="max-w-3xl">
+                        <p class="text-base font-semibold">Comprar Holi</p>
+                        <h2 class="mt-3 text-4xl font-extrabold tracking-tight text-zinc-950">Ver precios y opciones.</h2>
+                        <p class="mt-5 text-zinc-950/60">Comienza a acortar tus enlaces largos ahora.</p>
+                        <div class="mt-12 flex">
+                            <a href="/holi/comprar" class="flex h-14 items-center rounded-full bg-zinc-900 px-7 font-medium text-white hover:bg-zinc-800">
+                                Ver tarifas y comprar Holi
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </x-container>
     </main>
