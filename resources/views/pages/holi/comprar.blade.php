bc8918415cfbd1dcbf8b0976dfe4fca439f2e811
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-02-15 20:45:24 -0600

    wip

--- resources/views/pages/holi/comprar.blade.php
+++ resources/views/pages/holi/comprar.blade.php
@@ -3,19 +3,16 @@
     <x-holi.subnav />
     <main class="mt-6">
         <x-container>
-            <div class="py-20">
-                <h1 class="mt-4 text-6xl font-extrabold tracking-tight">Comprar Holi</h1>
-                <p class="mt-5 max-w-xl text-lg text-zinc-800">
+            <div class="py-24">
+                <h1 class="mt-4 text-6xl font-extrabold tracking-tight text-zinc-950">Comprar Holi</h1>
+                <p class="mt-5 max-w-xl text-lg text-zinc-950/80">
                     Para consultas sobre licencias, facturación y procesamiento seguro de pagos por favor, utiliza soporte
                     @radiocubito.com.
                 </p>
-            </div>
-        </x-container>
-        <x-container>
-            <div class="py-20">
+                <div class="py-9">
                 <div class="grid gap-8 lg:grid-cols-3">
                     <div class="rounded-lg border border-zinc-200 p-8">
-                        <p class="text-sm font-semibold text-indigo-600">Básico</p>
+                        <p class="text-sm font-semibold text-zinc-950">Básico</p>
                         <h5 class="mt-2 text-[32px] font-bold tracking-tight">
                             $490
                             <span class="text-sm">MXN/año</span>
@@ -26,7 +23,7 @@
                         </ul>
                         <div class="mt-16 flex">
                             <a
-                                href="https://holiing.com/register"
+                                href="https://holi.radiocubito.com/register"
                                 class="flex h-12 items-center justify-center rounded-md bg-zinc-950 px-6 font-medium text-white hover:bg-zinc-800"
                             >
                                 Comprar Holi
@@ -34,7 +31,7 @@
                         </div>
                     </div>
                     <div class="rounded-lg border border-zinc-200 p-8">
-                        <p class="text-sm font-semibold text-indigo-600">Pro</p>
+                        <p class="text-sm font-semibold text-zinc-950">Pro</p>
                         <h5 class="mt-2 text-[32px] font-bold tracking-tight">
                             $790
                             <span class="text-sm">MXN/año</span>
@@ -45,7 +42,7 @@
                         </ul>
                         <div class="mt-16 flex">
                             <a
-                                href="https://holiing.com/register"
+                                href="https://holi.radiocubito.com/register"
                                 class="flex h-12 items-center justify-center rounded-md bg-zinc-950 px-6 font-medium text-white hover:bg-zinc-800"
                             >
                                 Comprar Holi
@@ -53,6 +50,7 @@
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
         </x-container>
     </main>
--- resources/views/pages/holi/index.blade.php
+++ resources/views/pages/holi/index.blade.php
@@ -1,45 +1,61 @@
 <x-layouts.marketing>
     <x-header />
     <x-holi.subnav />
-    <main class="mt-6">
+    <main>
         <x-container>
-            <div class="py-20">
-                <h1 class="mt-4 text-6xl font-extrabold tracking-tight">
-                    Enlaces cortos,
-                    <br />
-                    al instante.
-                </h1>
-                <p class="mt-5 max-w-lg text-lg text-zinc-800">
-                    Transforma rápidamente URLs largas en cortas para distribuírlas más fácilmente en redes sociales.
-                </p>
-                <div class="flex pt-12">
-                    <a
-                        href="https://holi.radiocubito.com/register"
-                        class="flex h-14 items-center rounded-full bg-zinc-900 px-7 font-medium text-white hover:bg-zinc-800"
-                    >
-                        Comienza a acortar enlaces →
-                    </a>
+            <div class="pt-24 pb-16">
+                <div class="space-y-16">
+                    <div class="space-y-12">
+                        <div class="space-y-6">
+                            <h1 class="text-6xl font-extrabold tracking-tight text-zinc-950">
+                                Enlaces cortos,
+                                <br />
+                                al instante.
+                            </h1>
+                            <p class="max-w-lg text-lg text-zinc-950/80">
+                                Transforma rápidamente URLs largas en cortas para distribuírlas más fácilmente en redes sociales.
+                            </p>
+                        </div>
+                        <div class="flex">
+                            <a
+                                href="https://holi.radiocubito.com/register"
+                                class="flex h-14 items-center rounded-full bg-zinc-900 px-7 font-medium text-white hover:bg-zinc-800"
+                            >
+                                Comienza a acortar enlaces →
+                            </a>
+                        </div>
+                    </div>
+                    <div class="w-[45rem] overflow-hidden rounded-xl border border-zinc-200 bg-slate-50 shadow-xl shadow-zinc-950/20 sm:w-auto lg:w-[67.8125rem]">
+                        <img src="/images/screenshot-1.png" class="w-full">
+                    </div>
                 </div>
             </div>
         </x-container>
         <x-container>
             <div class="py-20">
-                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight">Personaliza tus enlaces cortos</h2>
-                <p class="mt-5 max-w-xl text-lg text-zinc-800">Utiliza tus propios dominios para crear URLs breves y que representan a tu marca o proyecto.</p>
+                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight text-zinc-950">Personaliza tus enlaces cortos</h2>
+                <p class="mt-5 max-w-xl text-lg text-zinc-950/80">Utiliza tus propios dominios para crear URLs breves y que representan a tu marca o proyecto.</p>
+
+                <div class="mt-10 w-[45rem] overflow-hidden rounded-xl border border-zinc-200 bg-slate-50 shadow-xl shadow-zinc-950/20 sm:w-auto lg:w-[67.8125rem]">
+                    <img src="/images/screenshot-2.png" class="w-full">
+                </div>
             </div>
         </x-container>
         <x-container>
             <div class="py-20">
-                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight">Previsualización social para tus enlaces</h2>
-                <p class="mt-5 max-w-xl text-lg text-zinc-800">
+                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight text-zinc-950">Previsualización social para tus enlaces</h2>
+                <p class="mt-5 max-w-xl text-lg text-zinc-950/80">
                     Asegura un impacto visual en tus redes sociales con vistas previas personalizadas para tus URLs acortadas.
                 </p>
+                <div class="mt-10 w-[45rem] overflow-hidden rounded-xl border border-zinc-200 bg-slate-50 shadow-xl shadow-zinc-950/20 sm:w-auto lg:w-[67.8125rem]">
+                    <img src="/images/screenshot-3.png" class="w-full">
+                </div>
             </div>
         </x-container>
         <x-container>
             <div class="py-20">
-                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight">Plataforma abierta de enlaces cortos</h2>
-                <p class="mt-5 max-w-xl text-lg text-zinc-800">
+                <h2 class="mt-4 max-w-lg text-5xl font-extrabold tracking-tight text-zinc-950">Plataforma abierta de enlaces cortos</h2>
+                <p class="mt-5 max-w-xl text-lg text-zinc-950/80">
                     Software de código abierto que garantiza la libertad de elección y evita dependencias innecesarias.
                 </p>
             </div>
