e958844b6e13267bf06d26c6b4850577328ab12b
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-02-17 12:38:58 -0600

    wip

---
+++ resources/views/components/home/hero.blade.php
@@ -0,0 +1,19 @@
+<section>
+    <div class="py-16 lg:py-24">
+        <x-container>
+            <div class="max-w-3xl">
+                <p class="font-extrabold text-zinc-950">Holi</p>
+                <h1 class="mt-3 text-5xl font-extrabold tracking-tight text-zinc-950 lg:text-6xl">
+                    Enlaces cortos,
+                    al instante.
+                </h1>
+                <p class="mt-6 text-lg text-zinc-950/80 lg:text-xl">
+                    Transforma rápidamente URLs largas en cortas para distribuírlas más fácilmente en redes sociales.
+                </p>
+                <div class="mt-12">
+                    <x-button href="/holi" color="green">Más información →</x-button>
+                </div>
+            </div>
+        </x-container>
+    </div>
+</section>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,26 +1,7 @@
 <x-layouts.marketing>
     <x-header />
-    <main class="mt-6">
-        <x-container>
-            <div class="pb-16 pt-24">
-                <div class="flex">
-                    <span
-                        class="inline-flex items-center gap-x-1.5 rounded-md bg-green-500/15 px-1.5 py-0.5 text-sm/5 font-medium text-green-700 sm:text-xs/5 forced-colors:outline"
-                    >
-                        Holi
-                    </span>
-                </div>
-                <h1 class="mt-4 text-6xl font-extrabold tracking-tight text-zinc-950">
-                    Enlaces cortos,
-                    <br />
-                    al instante.
-                </h1>
-                <p class="mt-6 max-w-lg text-lg">Transforma rápidamente URLs largas en cortas para distribuírlas más fácilmente en redes sociales.</p>
-                <div class="flex pt-12">
-                    <x-button href="/holi" color="green">Más información →</x-button>
-                </div>
-            </div>
-        </x-container>
+    <main>
+        <x-home.hero />
     </main>
     <x-footer />
 </x-layouts.marketing>
